af1b4b50459832efbd98c1750853594a588e303b
ThanhTai-IT <vovan.thanhtai7777@gmail.com>
2025-11-19 20:55:22 +0700

    done: thêm documentation cho utility function cn

--- medical_management_fe/src/lib/utils.ts
+++ medical_management_fe/src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Utility function để merge các className với Tailwind CSS
+ * Sử dụng clsx và tailwind-merge để xử lý conflict
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
