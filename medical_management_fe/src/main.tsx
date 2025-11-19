98ad08d27ce67f358e06ea018b9de2d6345fe9dd
ThanhTai-IT <vovan.thanhtai7777@gmail.com>
2025-11-19 20:52:44 +0700

    update: cải thiện cấu hình main entry point của ứng dụng

--- medical_management_fe/src/main.tsx
+++ medical_management_fe/src/main.tsx
@@ -7,6 +7,8 @@ import { ThemeProvider } from "./components/theme-provider";
 import { QueryProvider } from "./providers/QueryProvider";
 import { Toaster } from "react-hot-toast";
 
+// Cấu hình chính của ứng dụng React
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="aurora-ui-theme">
